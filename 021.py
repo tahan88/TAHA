19aba787d595af33dc1cc4f1b704bd3aa481c5b4
tahan88 <tahanzadehtaha@gmail.com>
2025-08-21 02:17:17 +0330

    Update 021.py

--- 021.py
+++ 021.py
@@ -1,1 +1,8 @@
 import os as sytm
+import random as sh
+a = int(input('یک عدد از 1تا 10 بگو'))
+x = sh.randint(1,10)
+if x==a:
+    print('شما برنده شدید !!!!!!!!!😁🤩')
+# else:
+#     sytm.remove("")
