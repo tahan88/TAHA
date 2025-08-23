c74780d7994b338f5b1463e10f8d533ade364cef
tahan88 <tahanzadehtaha@gmail.com>
2025-08-23 12:37:43 +0330

    Update 021.py

--- 021.py
+++ 021.py
@@ -1,8 +1,31 @@
-import random as shans
-x = shans.randint(1,6)
-y = shans.randint(1,6)
-a = x+y 
-if a ==7:
-    print('شما بردی!!!!!🤩')
-else:
-    print('شما باختی!!!😒')
+# import random as shans
+# x = shans.randint(1,6)
+# y = shans.randint(1,6)
+# a = x+y 
+# if a ==7:
+#     print('شما بردی!!!!!🤩')
+# else:
+#     print('شما باختی!!!😒')
+
+#  timerپروژه دوم
+
+# import time as timer
+# x = int(input('yek adad bego: '))
+# for i in range(x,-1,-1):
+#     print(i)
+#     timer.sleep(1)
+#     if i ==0:
+#         print('zaman tmam shod!!!!')
+
+# پروژه سوم تاریخ تولد
+
+# import datetime as th
+# y = th.datetime.date(input('تاریخ بگو'))
+# x = th.datetime.now
+# c = x-y
+
+# پروژه چهارم
+
+import os as fol
+x = fol.mkdir('notes')
+
